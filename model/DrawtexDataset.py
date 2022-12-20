a09bd77c077887d2be7544f6babf6ea13b0ad367
Benjamin Xia <benjx32@gmail.com>
2022-12-19 16:57:19 -0800

    custom pytorch dataset

---
+++ model/DrawtexDataset.py
@@ -0,0 +1,65 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import torch
+import os
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+
+
+class DrawtexDataset(Dataset):
+    classes: list[str] = ['!', '(', ')', '+', ',', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '=', 'A',
+                          'alpha', 'ascii_124', 'b', 'beta', 'C', 'cos', 'd', 'Delta', 'div', 'e', 'exists', 'f',
+                          'forall', 'forward_slash', 'G', 'gamma', 'geq', 'gt', 'H', 'i', 'in', 'infty', 'int', 'j',
+                          'k', 'l', 'lambda', 'ldots', 'leq', 'lim', 'log', 'lt', 'M', 'mu', 'N', 'neq', 'o', 'p',
+                          'phi', 'pi', 'pm', 'prime', 'q', 'R', 'rightarrow', 'S', 'sigma', 'sin', 'sqrt', 'sum', 'T',
+                          'tan', 'theta', 'times', 'u', 'v', 'w', 'X', 'y', 'z', '[', ']', '{', '}']
+    mapping: dict[str, int] = {'!': 0, '(': 1, ')': 2, '+': 3, ',': 4, '-': 5, '0': 6, '1': 7, '2': 8, '3': 9, '4': 10,
+                               '5': 11, '6': 12, '7': 13, '8': 14, '9': 15, '=': 16, 'A': 17, 'alpha': 18,
+                               'ascii_124': 19, 'b': 20, 'beta': 21, 'C': 22, 'cos': 23, 'd': 24, 'Delta': 25,
+                               'div': 26, 'e': 27, 'exists': 28, 'f': 29, 'forall': 30, 'forward_slash': 31, 'G': 32,
+                               'gamma': 33, 'geq': 34, 'gt': 35, 'H': 36, 'i': 37, 'in': 38, 'infty': 39, 'int': 40,
+                               'j': 41, 'k': 42, 'l': 43, 'lambda': 44, 'ldots': 45, 'leq': 46, 'lim': 47, 'log': 48,
+                               'lt': 49, 'M': 50, 'mu': 51, 'N': 52, 'neq': 53, 'o': 54, 'p': 55, 'phi': 56, 'pi': 57,
+                               'pm': 58, 'prime': 59, 'q': 60, 'R': 61, 'rightarrow': 62, 'S': 63, 'sigma': 64,
+                               'sin': 65, 'sqrt': 66, 'sum': 67, 'T': 68, 'tan': 69, 'theta': 70, 'times': 71, 'u': 72,
+                               'v': 73, 'w': 74, 'X': 75, 'y': 76, 'z': 77, '[': 78, ']': 79, '{': 80, '}': 81}
+
+    def __init__(self, transform=None):
+        self.data: np.ndarray = np.load("../data/data_matrix.npy")
+        self.labels: np.ndarray = np.load("../data/label_matrix.npy")
+        self.transform = transform
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, item: any) -> tuple[torch.Tensor, torch.Tensor]:
+        img = self.data[item]
+        label = self.labels[item]
+
+        if self.transform is not None:
+            img: torch.Tensor = self.transform(img)
+        else:
+            img: torch.Tensor = torch.from_numpy(img)
+
+        label = torch.tensor(label)
+        return img, label
+
+
+# Debugging stuff below
+if __name__ == "__main__":
+    training_set = DrawtexDataset(transforms.ToTensor())
+
+    train_load: DataLoader = DataLoader(
+        dataset=training_set,
+        batch_size=100,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=4
+    )
+
+    iter = enumerate(train_load)
+    idx, (img, lab) = next(iter)
+    print(training_set.classes[lab[0]])
+    plt.imshow(img[0][0])
+    plt.show()
--- processing/extract.py
+++ processing/extract.py
@@ -8,10 +8,12 @@ DATA_RES_Y = 45  # Resolution of each image vertically
 DATA_PATH = "../data/extracted_images"
 
 # Matrix of all images, data_matrix[i] is i'th image
-data_matrix = np.ndarray(shape=(NUM_DATA, DATA_RES_Y, DATA_RES_X, 1), dtype=np.uint8)
+data_matrix = np.ndarray(shape=(NUM_DATA, DATA_RES_Y, DATA_RES_X, 1),
+                         dtype=np.uint8)
 
 # Matrix of all image labels, label_matrix[i] is label of i'th image
-label_matrix = np.ndarray(shape=(NUM_DATA), dtype=np.uint32)
+label_matrix = np.ndarray(shape=(NUM_DATA),
+                          dtype=np.uint8)
 
 
 THRESH = 128 # Threshold for image grayscale -> B/W conversion
@@ -23,14 +25,12 @@ if __name__ == "__main__":  # String to index mapping
     i = 0
     for directory in classes:
         for file in os.listdir(f"{DATA_PATH}/{directory}"):
-            # Read image, convert to black and white
+            # Read image, convert to black and white and normalize from [0, 255] to [0, 1]
             img: np.ndarray = cv2.imread(f"{DATA_PATH}/{directory}/{file}")
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            img = cv2.threshold(img, THRESH, 255, cv2.THRESH_BINARY)[1]
+            img = cv2.threshold(img, THRESH, 1, cv2.THRESH_BINARY)[1]
             # Convert image shape to have 1 channel (standard image format)
             img = img.reshape((DATA_RES_Y, DATA_RES_X, 1))
-            # Normalize image to binary
-            img //= 255
             # Save to matrices
             data_matrix[i] = img
             label_matrix[i] = np.uint8(mappings[directory])
