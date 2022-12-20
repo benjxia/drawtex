89dd6823eca9732ba7094b91bff3487f21565c84
Benjamin Xia <benjx32@gmail.com>
2022-12-20 02:31:33 -0800

    drawtex model v2.1

--- model/DrawtexModel.py
+++ model/DrawtexModel.py
@@ -6,17 +6,17 @@ class DrawTexModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.relu = nn.ReLU()
-        self.conv1 = nn.Conv2d(1, 200, 9, bias=False)  # 45x45 -> 37x37
+        self.conv1 = nn.Conv2d(1, 100, 9, bias=False)  # 45x45 -> 37x37
         self.conv1_bn = nn.BatchNorm2d(200)
-        self.conv2 = nn.Conv2d(200, 400, 9, bias=False)  # 37x37 -> 29x29
+        self.conv2 = nn.Conv2d(200, 300, 9, bias=False)  # 37x37 -> 29x29
         self.conv2_bn = nn.BatchNorm2d(400)
-        self.conv3 = nn.Conv2d(400, 800, 9, bias=False)  # 29x29 -> 21x21
+        self.conv3 = nn.Conv2d(300, 500, 9, bias=False)  # 29x29 -> 21x21
         self.conv3_bn = nn.BatchNorm2d(800)
-        self.conv4 = nn.Conv2d(800, 1200, 9, bias=False)  # 21x21 -> 13x13
+        self.conv4 = nn.Conv2d(500, 800, 9, bias=False)  # 21x21 -> 13x13
         self.conv4_bn = nn.BatchNorm2d(1200)
-        self.conv5 = nn.Conv2d(1200, 1800, 9, bias=False)  # 13x13 -> 5x5
+        self.conv5 = nn.Conv2d(800, 1000, 9, bias=False)  # 13x13 -> 5x5
         self.conv5_bn = nn.BatchNorm2d(1800)
-        self.lin1 = nn.Linear(45000, 82, bias=False)
+        self.lin1 = nn.Linear(25000, 82, bias=False)
         self.lin1_bn = nn.BatchNorm1d(82)
 
     def forward(self, x: torch.Tensor):
