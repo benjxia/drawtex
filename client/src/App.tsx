329bd80f397d2a227d6fc634002aeb2f4c80ee42
Benjamin Xia <benjx32@gmail.com>
2023-01-09 16:12:22 -0800

    begin website portion

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+          <br></br>
+          <code>das;lfkja;lsdkjf;alsdkjf</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
