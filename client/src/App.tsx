c1c7700950e23a384b3a275d0b2961cd108386bf
Benjamin Xia <benjx32@gmail.com>
2023-01-14 21:00:04 -0800

    added drawing canvas to front page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
+import CanvasDraw from "react-canvas-draw";
 
 function App() {
+  let i: JSX.Element = <CanvasDraw />
+  
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
           <br></br>
-          <code>das;lfkja;lsdkjf;alsdkjf</code>
+          {i}
         </p>
       </header>
     </div>
   );
 }
-
+// TODO: switch to es6?
 export default App;
