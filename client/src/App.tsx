d6b59351d1954f41e8153b8fe1128543442c9b02
Benjamin Xia <benjx32@gmail.com>
2023-01-15 15:30:07 -0800

    get image url after update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,8 +12,13 @@ export default class App extends Component {
     this.canvas = null;
   }
 
-  hello(): void {
-    console.log("hi");
+  /**
+   * Records the current state of the canvas as a URL
+   * before feeding it into the model
+   */
+  updatePred(): void {
+    // @ts-ignore: Unreachable code error
+    let img_url: string = this.canvas?.getDataURL();
   }
 
   render() {
@@ -24,7 +29,11 @@ export default class App extends Component {
             Edit <code>src/App.tsx</code> and save to reload.
             <br></br>
             <CanvasDraw ref= {CanvasDraw => (this.canvas = CanvasDraw)} 
-                        onChange={() => this.hello()}/>
+                        onChange={() => this.updatePred()}
+                        hideGrid={true}
+                        brushColor={"#000000"}
+                        lazyRadius={0}
+            />
           </p>
         </header>
       </div>
