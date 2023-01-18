e4a3e6b6a7664d6c23af049053406f6e4676e8b0
Benjamin Xia <benjx32@gmail.com>
2023-01-17 23:46:48 -0800

    tiny tf port progress

--- model/tensorflow/DT_Model.py
+++ model/tensorflow/DT_Model.py
@@ -1,9 +1,22 @@
 import tensorflow as tf
 from tensorflow.python import keras
-from keras import layers
+from keras import Model, layers
 
-model: keras.Sequential = keras.Sequential()
+class DT_Model_tf(Model):
+    def __init__(self):
+        super(DT_Model_tf, self).__init__()
+        
+        self.conv1 = layers.Conv2D(filters=64,
+                                   kernel_size=4,
+                                   activation="relu",
+                                   input_shape=(45, 45, 1)
+        )
 
-model.add
-
-print(model)
+        # self.norm1 = layers.norm
+    
+    def call(self, x, train=False):
+        pass
+model: DT_Model_tf = DT_Model_tf()
+# model.add
+model.build(input_shape=(45, 45, 1))
+print(model.summary())
