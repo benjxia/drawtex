de0975bf76fcf38135c98a2f13596b22de3fb34b
Benjamin Xia <benjx32@gmail.com>
2023-01-18 21:09:32 -0800

    define 1 more layer, snail pace

--- model/tensorflow/DT_Model.py
+++ model/tensorflow/DT_Model.py
@@ -9,13 +9,24 @@ class DT_Model_tf(Model):
         self.conv1 = layers.Conv2D(filters=64,
                                    kernel_size=4,
                                    activation="relu",
-                                   input_shape=(45, 45, 1)
-        )
+                                    )
+
+        self.pool1 = layers.MaxPooling2D(pool_size=(2, 2))
 
         # self.norm1 = layers.norm
-    
+        self.conv2 = layers.Conv2D(filters=128,
+                                   kernel_size=4,
+                                   activation="relu",
+                                    )
+        
+
     def call(self, x, train=False):
-        pass
+        x = tf.reshape(x, [-1, 45, 45, 1])
+        x = self.conv1(x)
+        # x = self.pool1(x)
+        return x
+
+
 model: DT_Model_tf = DT_Model_tf()
 # model.add
 model.build(input_shape=(45, 45, 1))
